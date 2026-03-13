795be45121ac5a79535935dbacc444d603048622
outlyr-in <outlyr.official@gmail.com>
2026-03-13 16:21:29 +0100

    fix: add site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://www.outlyr.in',
   output: 'server',
   adapter: vercel(),
   integrations: [react(), keystatic()],
